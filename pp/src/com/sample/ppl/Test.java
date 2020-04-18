b62a6e1241edf38e23f61a5b9ebfd17784814dbd
ankit127 <ankitkothawade14@gmail.com>
2020-04-18 17:44:07 +0530

    Test Class is Updated - Added ArrayList TO Display Name

--- pp/src/com/sample/ppl/Test.java
+++ pp/src/com/sample/ppl/Test.java
@@ -1,9 +1,19 @@
 package com.sample.ppl;
 
+import java.util.ArrayList;
+
 public class Test {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		
+		ArrayList<String> arr = new ArrayList<String>();
+		
+		arr.add("Ankit");
+		arr.add("Mohan");
+		arr.add("Wani");
+		
+		System.out.println(arr);
 
 	}
 
